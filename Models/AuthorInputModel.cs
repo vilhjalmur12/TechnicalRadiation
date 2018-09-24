022d50336abdc55bb58efc5fd4d97e0df86a48c3
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-09-24 19:37:26 +0000

    Added Author models, data validation in Author input model

---
+++ Models/AuthorInputModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalRadiation.Models
+{
+    public class AuthorInputModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Url]
+        public string ProfileImgSource { get; set; }
+
+        [MaxLength(255)]
+        public string Bio { get; set; }
+    }
+}
