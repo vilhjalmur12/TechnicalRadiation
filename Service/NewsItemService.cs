5724a813b62ea0c8bb1f8d87839fc37a4d863c98
villi <vilhjalmur12@ru.is>
2018-09-27 15:15:36 +0000

    Merge pull request #3 from vilhjalmur12/villi-services
    getAllNewsDto

---
+++ Service/NewsItemService.cs
@@ -0,0 +1,13 @@
+using TechnicalRadiation.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TechnicalRadiation.Service
+{
+    public interface NewsItemService
+    {
+        List<NewsItemDto> getLightweight();
+        
+         
+    }
+}
