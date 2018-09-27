bf0bd18b7493995901661c4c0b9d48ddc28bdf77
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-09-27 14:21:23 +0000

    Populated _newsmodel

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -7,7 +7,7 @@ namespace TechnicalRadiation.Data
 {
     public static class DbContext
     {
-        private static List<NewsItem> _models = new List<NewsItem>
+        private static List<NewsItem> _newsmodels = new List<NewsItem>
         {
             new NewsItem
             {
@@ -35,6 +35,69 @@ namespace TechnicalRadiation.Data
                 ShortDescription = "A short description",
                 LongDescription = "A long description",
                 PublishDate = DateTime.Parse("24/07/2018 10:00:03"),
+            },
+            new NewsItem
+            {
+                Id = 4,
+                Title = "Title of the news article 4",
+                ImgSoruce = "https://example.com/news-item/4.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:04"),
+            },
+            new NewsItem
+            {
+                Id = 5,
+                Title = "Title of the news article 5",
+                ImgSoruce = "https://example.com/news-item/5.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:05"),
+            },
+            new NewsItem
+            {
+                Id = 6,
+                Title = "Title of the news article 6",
+                ImgSoruce = "https://example.com/news-item/6.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:06"),
+            },
+            new NewsItem
+            {
+                Id = 7,
+                Title = "Title of the news article 7",
+                ImgSoruce = "https://example.com/news-item/7.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:07"),
+            },
+            new NewsItem
+            {
+                Id = 8,
+                Title = "Title of the news article 8",
+                ImgSoruce = "https://example.com/news-item/8.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:08"),
+            },
+            new NewsItem
+            {
+                Id = 9,
+                Title = "Title of the news article 9",
+                ImgSoruce = "https://example.com/news-item/9.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:09"),
+            },
+            new NewsItem
+            {
+                Id = 10,
+                Title = "Title of the news article 10",
+                ImgSoruce = "https://example.com/news-item/10.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                PublishDate = DateTime.Parse("24/07/2018 10:00:10"),
             },
         };
     }
